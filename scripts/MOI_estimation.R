28b7dd28189ebbb10255d3ee8cdc8df65af47e44
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2023-08-02 19:16:11 -0400

    Update MOI_estimation.R

--- scripts/MOI_estimation.R
+++ scripts/MOI_estimation.R
@@ -7,8 +7,14 @@ suppressPackageStartupMessages({
 option_list = list(
   make_option(c("--input"), type="character", default=NULL,
               help="path and name of the input file", metavar="character"),
+  make_option(c("--prior"), type="character", default="uniform",
+              help="user-specified prior for MOI distribution [default= %default]; two options, either a uniform or a negative binomial (negBinom)", metavar="character"),
+  make_option(c("--params"), type="character", default="medium",
+              help="when specifying a negative binomial prior, providing a parameter range for it [default= %default]; three options, low or medium or high, corresponding to a negative binomial with a low (~1), medium (~4), and high (~7) mean value", metavar="character"),
+  make_option(c("--verbose"), type="logical", default=TRUE,
+              help="whether output the prior distribution for MOI estimation or not [default= %default]", metavar="character"),
   make_option(c("--aggregate"), type="character", default="pool",
-              help="how to obtain the MOI distribution at the population level from individual MOI estimates", metavar="character"),
+              help="how to obtain the MOI distribution at the population level from individual MOI estimates; two options, pool or mixtureDist, either pooling the maximum a posteriori MOI estimate for each individual host together, or using mixture distribution, i.e., a weighted summation of all individual MOI distributions", metavar="character"),
   make_option(c("--util"), type="character", default="./s_givenMOI_list",
               help="the list which stores the probability distribution of the number of non-upsA DBLa types given any MOI", metavar="character"),
   make_option(c("--output"), type="character", default="./out.txt",
@@ -27,8 +33,36 @@ utilFile <- opt$util
 load(utilFile)
 p_s_givenMOI <- s_givenMOI_list
 
-MOI_priors <- rep(1/MOI_max, MOI_max)
-names(MOI_priors) <- as.character(1:MOI_max)
+
+simNegBinomPrior <- function(n, prob, MOI_min = 1, MOI_max = 20) {
+  baseline <- 0.00001
+  MOI_priors_test <- rnbinom(20000, size = n, prob = prob)
+  MOI_priors_test <- MOI_priors_test[MOI_priors_test>=MOI_min & MOI_priors_test <= MOI_max]
+  MOI_priors_test <- prop.table(table(MOI_priors_test))
+  labels <- rownames(MOI_priors_test)
+  MOI_priors <- rep(baseline, MOI_max)
+  names(MOI_priors) <- as.character(1:MOI_max)
+  MOI_priors[labels] <- MOI_priors_test + baseline
+  MOI_priors <- MOI_priors/sum(MOI_priors)
+  return(MOI_priors)
+} 
+
+
+if (opt$prior == "uniform") {
+  MOI_priors <- rep(1/MOI_max, MOI_max)
+  names(MOI_priors) <- as.character(1:MOI_max)
+} else if (opt$prior == "negBinom") {
+  if (opt$params == "low") {
+    set.seed(0)
+    MOI_priors <- simNegBinomPrior(n = 1, prob = 0.75, MOI_max = MOI_max)
+  } else if (opt$params == "medium") {
+    set.seed(1)
+    MOI_priors <- simNegBinomPrior(n = 5, prob = 0.55, MOI_max = MOI_max)
+  } else if (opt$params == "high") {
+    set.seed(2)
+    MOI_priors <- simNegBinomPrior(n = 4, prob = 0.35, MOI_max = MOI_max)
+  }
+}
 
 p <- p_s_givenMOI[[1]]
 s_single <- max(45, b)
@@ -74,8 +108,16 @@ for (j in 1:length(s_temp)) {
 if (opt$aggregate == "pool") {
   dfAllPop <- dfAll %>% group_by(MOI) %>% summarise("n" = n()) %>% mutate("n_total" = sum(n), "prob" = n/n_total) %>% select(MOI, prob)
 } else if (opt$aggregate == "mixtureDist") {
-  dfAllPop <- dfAll %>% group_by(MOI) %>% summarise("n" = sum(p)) %>% mutate("n_total" = sum(n), "prob" = n/n_total)
+  dfAllPop <- dfAll %>% group_by(MOI) %>% summarise("n" = sum(p)) %>% mutate("n_total" = sum(n), "prob" = n/n_total) %>% select(MOI, prob)
+} else if (opt$aggregate == "mixtureDist") {
 }
-outputList <- list("indLevel" = dfAll,
-                   "popLevel" = dfAllPop)
+if (opt$verbose == TRUE) {
+  outputList <- list("prior" = MOI_priors,
+                     "indLevel" = dfAll,
+                     "popLevel" = dfAllPop)
+} else {
+  outputList <- list("indLevel" = dfAll,
+                     "popLevel" = dfAllPop)
+}
+
 save(outputList, file = opt$output)
